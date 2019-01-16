155b488f1b24a5056b03130ee6e06a07aab1c608
hypernova1 <chtlstjd01@gmail.com>
2019-01-17 08:50:57 +0900

    .

--- Guiders/src/main/java/com/guiders/web/member/domain/MemberVO.java
+++ Guiders/src/main/java/com/guiders/web/member/domain/MemberVO.java
@@ -2,4 +2,61 @@ package com.guiders.web.member.domain;
 
 public class MemberVO {
 
+  private String email;
+  private String password;
+  private String ename;
+  private int gender;
+  private String phone;
+  private String photo;
+  private int ctno;
+  private String regdate;
+  
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getEname() {
+    return ename;
+  }
+  public void setEname(String ename) {
+    this.ename = ename;
+  }
+  public int getGender() {
+    return gender;
+  }
+  public void setGender(int gender) {
+    this.gender = gender;
+  }
+  public String getPhone() {
+    return phone;
+  }
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+  public String getPhoto() {
+    return photo;
+  }
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+  public int getCtno() {
+    return ctno;
+  }
+  public void setCtno(int ctno) {
+    this.ctno = ctno;
+  }
+  public String getRegdate() {
+    return regdate;
+  }
+  public void setRegdate(String regdate) {
+    this.regdate = regdate;
+  }
 }
