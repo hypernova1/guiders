f9d1695a93526a2c07004f041d0c5689275b959d
hypernova1 <chtlstjd01@gmail.com>
2021-02-22 21:29:37 +0900

    test: 테스트 추가

---
+++ src/test/java/org/brokers/guiders/api/MemberControllerTest.java
@@ -0,0 +1,27 @@
+package org.brokers.guiders.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test() throws Exception {
+        mockMvc.perform(get("/api/user/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
