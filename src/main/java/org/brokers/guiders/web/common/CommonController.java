5cda85fb2eee9b973a06717e7283419bb2c6bb60
hypernova1 <chtlstjd01@gmail.com>
2021-03-02 22:16:17 +0900

    refactor: 불필요 api 제거

--- src/main/java/org/brokers/guiders/web/common/CommonController.java
+++ src/main/java/org/brokers/guiders/web/common/CommonController.java
@@ -37,64 +37,6 @@ public class CommonController {
         return "main";
     }
 
-    @PostMapping("/doA")
-    public String doA(Locale locale, Model model, Authentication authentication, 
-    		@RequestBody String title){
-//        JSONObject cred = new JSONObject();
-//        JSONObject auth=new JSONObject();
-//        JSONObject parent=new JSONObject();
-//		if (authentication != null) {
-//			UserCustom userCustom = (UserCustom) authentication.getPrincipal();
-//			String userName = userCustom.getUsername();
-//			cred.put("username",userName);
-//		}
-//        cred.put("title", title);
-//        auth.put("userInfo", cred);
-//        parent.put("auth", auth);
-//
-//        URLConn conn = new URLConn("http://127.0.0.1", 1516);
-//        conn.urlPost(parent);
-
-        return "main";
-    }
-
-    // 다중파일업로드
-    @PostMapping("/file_uploader_html5.do")
-    @ResponseBody
-    public String multiplePhotoUpload(HttpServletRequest request) {
-        // 파일정보
-        StringBuffer sb = new StringBuffer();
-        try {
-            // 일반 원본파일명
-            String oldName = request.getHeader("file-name");
-            // 파일 기본경로 _ 상세경로
-
-            /* 배포시 실제로 저장될 경로 지정 */
-            String filePath = servletContext.getRealPath("/resources/img/photoUpload/");
-
-            String saveName =
-                    sb.append(new SimpleDateFormat("yyyyMMddHHmmss").format(System.currentTimeMillis()))
-                            .append(UUID.randomUUID().toString())
-                            .append(oldName.substring(oldName.lastIndexOf("."))).toString();
-            InputStream is = request.getInputStream();
-            OutputStream os = new FileOutputStream(filePath + saveName);
-            int numRead;
-            byte[] b = new byte[Integer.parseInt(request.getHeader("file-size"))];
-            while ((numRead = is.read(b, 0, b.length)) != -1) {
-                os.write(b, 0, numRead);
-            }
-            os.flush();
-            os.close();
-            // 정보 출력
-            sb = new StringBuffer();
-            sb.append("&bNewLine=true").append("&sFileName=").append(oldName).append("&sFileURL=")
-                    .append("/resources/img/photoUpload/").append(saveName); // 서버쪽 주소를 기준으로...
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
-
     @PostMapping("/uploadImage")
     @ResponseBody
     public String uploadImage(MultipartFile file) throws Exception {
